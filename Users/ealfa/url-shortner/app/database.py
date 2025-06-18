95c134afec689168d87dd1c02a1d71e4c96b76ee
Ehimenose Aigbe <ehialfaigbe@gmail.com>
2025-06-17 23:06:05 -0400

    Added sql database instead of tracking with memory

---
+++ Users/ealfa/url-shortner/app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 1. Connect to SQLite
+DATABASE_URL = "sqlite:///./shortener.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+
+# 2. Create session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 3. Base class
+Base = declarative_base()
+
+# 4. Define the table model
+class URL(Base):
+    __tablename__ = "urls"
+    short_id = Column(String, primary_key=True, index=True)
+    original_url = Column(String, nullable=False)
--- Users/ealfa/url-shortner/app/main.py
+++ Users/ealfa/url-shortner/app/main.py
@@ -3,6 +3,20 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from fastapi.responses import RedirectResponse
 from fastapi import HTTPException
+from app.database import SessionLocal, URL
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from app.database import Base, engine
+Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 app = FastAPI()
 
@@ -16,24 +30,32 @@ def read_root():
     return{"message": "Starting URL Shortener"}
 
 @app.get("/{short_id}")
-def redirect_to_url(short_id: str):
-    if short_id in url_mapping:
-        long_url = url_mapping[short_id]
-        return RedirectResponse(long_url)
+def redirect_to_url(short_id: str, db: Session = Depends(get_db)):
+    result = db.query(URL).filter(URL.short_id == short_id).first()
+    if result:
+        return RedirectResponse(result.original_url)
     
-    else:
+    else: 
 
         raise HTTPException(status_code = 404, detail = "Short URL not found")
 
 
 @app.post("/shorten")   
-def create_short_url(request_data: URLRequest, request: Request):
+def create_short_url(
+    request_data: URLRequest, 
+    request: Request,
+    db: Session = Depends(get_db)
+):
+
     long_url = request_data.url
     short_id = uuid.uuid4().hex[:6]
 
+    db_url = URL(short_id=short_id, original_url=long_url)
+    db.add(db_url)
+    db.commit()
+
     url_mapping[short_id] = long_url
 
-    base_url = str(request.base_url)
-    short_url = base_url + short_id
-
+    
+    short_url = str(request.base_url) + short_id
     return {"short_url": short_url}
